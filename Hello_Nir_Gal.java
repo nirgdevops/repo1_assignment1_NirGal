6d07dcb255050c7acb1fdc0326540afa16b9fe3b
nirgdevops <nirgdevops@gmail.com>
2020-12-27 20:19:03 +0200

    Commit 3

--- Hello_Nir_Gal.java
+++ Hello_Nir_Gal.java
@@ -3,7 +3,7 @@ public class Hello{
   for (int i=1;i<=10; i++) {
   System.out.println("Hello World ..."+i);
   }
-  System.out.println("Success number 2");
+  System.out.println("Success number 3");
   }
 }
 
